fe02238d3dff29be8984f47746cb3bb13a8d1faf
Csaboo64 <96649685+Csaboo64@users.noreply.github.com>
2023-12-18 12:56:54 +0100

    max gomb

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 function validateInput() {
     let inputElem = document.getElementById("szamInput");
     let inputValue = inputElem.value.trim();
-
+ 
     if (/^\d+$/.test(inputValue)) {
       inputElem.classList.remove("invalid");
     } else {
       inputElem.classList.add("invalid");
     }
   }
-
+ 
+ 
   function hozzaad() {
     let inputElem = document.getElementById("szamInput");
     let inputValue = inputElem.value.trim();
-
+ 
     if (/^\d+$/.test(inputValue)) {
       let listaElem = document.createElement("li");
       listaElem.textContent = inputValue;
-
+ 
       document.getElementById("szamLista").appendChild(listaElem);
-
+ 
       inputElem.value = "";
       inputElem.classList.remove("invalid");
     } else {
       inputElem.classList.add("invalid");
     }
   }
-
-  function kiszamolMinimum() {
+ 
+ 
+  function kiszamolMaximum() {
     let listaElemek = document.querySelectorAll("#szamLista li");
     let szamok = Array.from(listaElemek).map(li => parseInt(li.textContent));
-
+ 
     if (szamok.length > 0) {
-      let minimum = Math.min(...szamok);
-      document.getElementById("minimumEredmeny").textContent = `A legkisebb szám: ${minimum}`;
+      let maximum = Math.max(...szamok);
+      document.getElementById("maximumEredmeny").textContent = `A legnagyobb szám: ${maximum}`;
     } else {
-      document.getElementById("minimumEredmeny").textContent = "Nincs elérhető szám a listában.";
+      document.getElementById("maximumEredmeny").textContent = "Nincs elérhető szám a listában.";
     }
   }
